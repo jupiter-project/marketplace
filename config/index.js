7bd584bb55c80ed8c2b212495cfc00e275d18b6f
Great Dolphin <great.dolphin.ls@gmail.com>
2021-03-22 23:41:18 -0700

    [feat] update api-jupiter

--- config/index.js
+++ config/index.js
@@ -3,11 +3,9 @@ const PROXY_URL = process.env.NODE_ENV === 'production'
   ? 'https://leda.gojupiter.tech/'
   : 'http://localhost:8000'
 
-const JUPITER_URL = 'https://jpr4.gojupiter.tech'
-const JUPITER_FEE_CALCULATE_URL = '&feeNQT=0&broadcast=false&calculateFee=true';
+const JUPITER_URL = 'https://nodes.gojupiter.tech/'
 
 export {
   PROXY_URL,
   JUPITER_URL,
-  JUPITER_FEE_CALCULATE_URL
 }
--- services/api-jupiter.js
+++ services/api-jupiter.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-import { JUPITER_URL, JUPITER_FEE_CALCULATE_URL } from 'config'
+import { JUPITER_URL } from 'config'
 
 const apiAxios = axios.create({
   baseURL: JUPITER_URL,
@@ -50,93 +50,46 @@ const getDGSPendingPurchases = async (seller) => {
 }
 
 const setAccountInfo = async (params) => {
-  const defaultURL = `/nxt?requestType=setAccountInfo&name=${params.name}&description=${params.description}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=setAccountInfo&name=${params.name}&description=${params.description}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
 const createNFTToken = async (params) => {
-  const defaultURL = `/nxt?requestType=dgsListing&name=${params.name}&description=${params.description}&tags=${params.tags}&quantity=${params.quantity}&priceNQT=${params.price}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=dgsListing&name=${params.name}&description=${params.description}&tags=${params.tags}&quantity=${params.quantity}&priceNQT=${params.price}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
 const purchaseDGSGood = async (params) => {
-  const defaultURL = `/nxt?requestType=dgsPurchase&goods=${params.goods}&priceNQT=${params.priceNQT}&quantity=${params.quantity}&deliveryDeadlineTimestamp=${params.deliveryDeadlineTimestamp}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=dgsPurchase&goods=${params.goods}&priceNQT=${params.priceNQT}&quantity=${params.quantity}&deliveryDeadlineTimestamp=${params.deliveryDeadlineTimestamp}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
 const changeDGSGoodPrice = async (params) => {
-  const defaultURL = `/nxt?requestType=dgsPriceChange&goods=${params.goods}&priceNQT=${params.priceNQT}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=dgsPriceChange&goods=${params.goods}&priceNQT=${params.priceNQT}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
 const changeDGSGoodQuantity = async (params) => {
-  const defaultURL = `/nxt?requestType=dgsQuantityChange&goods=${params.goods}&deltaQuantity=${params.quantity}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=dgsQuantityChange&goods=${params.goods}&deltaQuantity=${params.quantity}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
 const deleteNFTToken = async (params) => {
-  const defaultURL = `/nxt?requestType=dgsDelisting&goods=${params.goods}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=dgsDelisting&goods=${params.goods}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
 const deliveryDGSGood = async (params, discountNQT) => {
-  let defaultURL = `/nxt?requestType=dgsDelivery&purchase=${params.purchase}&goodsToEncrypt=${params.goodsToEncrypt}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
+  let url = `/nxt?requestType=dgsDelivery&purchase=${params.purchase}&goodsToEncrypt=${params.goodsToEncrypt}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   if (!!discountNQT) {
-    defaultURL += `&discountNQT=${discountNQT}`
+    url += `&discountNQT=${discountNQT}`
   }
 
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
   return await apiAxios.post(url)
 }
 
 const refundDGSGood = async (params) => {
-  const defaultURL = `/nxt?requestType=dgsRefund&purchase=${params.purchase}&refundNQT=${params.refundNQT}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24`;
-
-  const feeNQTURL = `${defaultURL}${JUPITER_FEE_CALCULATE_URL}`;
-  const response = await apiAxios.post(feeNQTURL)
-
-  const { transactionJSON: { feeNQT = 0 } = {} } = response;
-  const url = `${defaultURL}&feeNQT=${feeNQT}`;
+  const url = `/nxt?requestType=dgsRefund&purchase=${params.purchase}&refundNQT=${params.refundNQT}&secretPhrase=${params.secretPhrase}&publicKey=${params.publicKey}&deadline=24&feeNQT=0`;
   return await apiAxios.post(url)
 }
 
