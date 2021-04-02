b943e3eda733529739cb66a415bcedfbba8378c1
Great Dolphin <great.dolphin.ls@gmail.com>
2021-04-02 01:59:22 -0700

    [feat] change default-image

--- utils/constants/common.js
+++ utils/constants/common.js
@@ -1,13 +1,14 @@
 
-const TITLE = 'Leda - Jupiter NFT Market';
-const SITE_URL = 'https://jupiter-frontend.vercel.app/';
-const DESCRIPTION = 'Join and Check New NFT of Leda';
-const PLACE_FILLER_URL = 'https://gojupiter.tech/placefiller';
+const TITLE = 'Leda - Jupiter NFT Market'
+const SITE_URL = 'https://jupiter-frontend.vercel.app'
+const DESCRIPTION = 'Join and Check New NFT of Leda'
+
+const PLACE_FILLER_URL = 'https://gojupiter.tech/placefiller'
 const UNISWAP_URL = 'https://app.uniswap.org/#/swap'
-const PANCAKESWAP_URL = 'https://pancakeswap.finance/'
+const PANCAKESWAP_URL = 'https://pancakeswap.finance'
+
 const NQT_WEIGHT = 100000000;
 const MAX_UPLOAD_SIZE = 52428800;
-const DEFAULT_IMAGE = 'https://res.cloudinary.com/leda/image/upload/v1615822449/l1aqnas6az50aclrtgh6.gif'
 
 export {
   TITLE,
@@ -17,6 +18,5 @@ export {
   UNISWAP_URL,
   PANCAKESWAP_URL,
   NQT_WEIGHT,
-  MAX_UPLOAD_SIZE,
-  DEFAULT_IMAGE
+  MAX_UPLOAD_SIZE
 }
