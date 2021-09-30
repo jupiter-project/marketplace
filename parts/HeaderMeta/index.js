912f6b699315ee49f926cb7f567b9677afae8f9a
greatdolphinls <thmsshanahan0815@gmail.com>
2021-09-30 16:11:31 +0000

    [feat] fix meta

--- parts/HeaderMeta/index.js
+++ parts/HeaderMeta/index.js
@@ -17,7 +17,7 @@ const HeaderMeta = () => {
       <meta property='twitter:url' content={COMMON_CONSTANTS.SITE_URL} />
       <meta property='twitter:title' content={COMMON_CONSTANTS.TITLE} />
       <meta property='twitter:description' content={COMMON_CONSTANTS.DESCRIPTION} />
-      <meta name='twitter:image' content={BANNER_IMAGE_PATH} />
+      <meta property='twitter:image' content={BANNER_IMAGE_PATH} />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   )
